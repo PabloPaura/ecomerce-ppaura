9c8467e8020d4e99dc93c9e0cdb67b944de65fe3
PabloPaura <pablodpaura@gmail.com>
2022-10-29 20:18:07 -0300

    deploy settings

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   // }, [])
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/ecomerce-ppaura" >
       <CartContextProvider>
         <NavBar />
         <Routes>
